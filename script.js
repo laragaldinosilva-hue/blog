5c57eed0629b9368acc25a76895961f2f845d7b7
laragaldinosilva-hue <lara.galdino.silva@escola.pr.gov.br>
2026-08-27 23:43:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-```javascript
-// MODO ESCURO
-
 const botaoTema = document.getElementById("theme-toggle");
 
 botaoTema.onclick = function () {
-
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-        botaoTema.innerHTML = "☀️ Modo claro";
+        botaoTema.textContent = "☀️ Modo claro";
     } else {
-        botaoTema.innerHTML = "🌙 Modo escuro";
+        botaoTema.textContent = "🌙 Modo escuro";
     }
-
 };
 
-
-// CURTIDAS
 
 const botoes = document.querySelectorAll(".reaction-button");
 
@@ -29,20 +22,13 @@ botoes.forEach(function (botao) {
         const numero = botao.querySelector("span");
 
         if (curtiu === false) {
-
-            numero.innerHTML = Number(numero.innerHTML) + 1;
-
+            numero.textContent = Number(numero.textContent) + 1;
             curtiu = true;
-
         } else {
-
-            numero.innerHTML = Number(numero.innerHTML) - 1;
-
+            numero.textContent = Number(numero.textContent) - 1;
             curtiu = false;
-
         }
 
     };
 
 });
-```
